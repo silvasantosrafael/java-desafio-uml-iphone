b7916066e5ca4f9e85c50567f219f25142945e95
silvasantosrafael <rafael.silva318819@gmail.com>
2023-09-24 23:57:01 -0300

    initial commit

---
+++ src/org/apple/interfaces/ReprodutorMusical.java
@@ -0,0 +1,4 @@
+package org.apple.interfaces;
+
+public interface ReprodutorMusical {
+}
---
+++ src/org/apple/model/IPhone.java
@@ -0,0 +1,8 @@
+package org.apple.model;
+
+import org.apple.interfaces.AparelhoTelefonico;
+import org.apple.interfaces.NavegadorInternet;
+import org.apple.interfaces.ReprodutorMusical;
+
+public class IPhone implements AparelhoTelefonico, ReprodutorMusical, NavegadorInternet {
+}
